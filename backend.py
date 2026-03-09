0ffc58e9939cf7b07f1e4cb8330d7714e36f1428
Luna Nia <wamasterzofwiis@gmail.com>
2026-03-09 13:18:37 +0100

    n

--- backend.py
+++ backend.py
@@ -2,7 +2,7 @@
 
 # Product: id, name, category, quantity, price
 # Order: id, list[Product], list[Quantity], date
-# Transaction: id, type (sale, return, transfer), quantity,
+# Transaction: id, type (sale, return, transfer), quantity, date
 
 # idk how to do the database stuff so that's on y'all to do thanks
 
